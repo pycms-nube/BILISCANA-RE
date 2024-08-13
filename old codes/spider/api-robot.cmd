0eed8ec5938d282a44b98594149de54aa2cc47da
PYCMS <2036382041@qq.com>
2024-08-13 16:06:38 -0700

    Remove not needed files

--- old codes/spider/api-robot.cmd
+++
@@ -1,6 +0,0 @@
-
-@echo off
-rem This script was created by Nuitka to execute 'api-robot.exe' with Python DLL being found.
-set PATH=c:\PROGRA~3\MINICO~1\envs\web-comment-ana;%PATH%
-"%~dp0.\api-robot.exe"
-
